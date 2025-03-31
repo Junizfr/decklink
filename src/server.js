4465ce6348eedf6dcebc63b561c16fa7f32ee07f
RosaiaNassim <rosaia244@gmail.com>
2025-03-31 13:20:21 +0200

    feat(mongodb): adding MongoDB and DefaultSchema

--- src/server.js
+++ src/server.js
@@ -1,14 +1,34 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const mongo = mongoose.connect(process.env.MONGO_URI);
+
+const initSchema = () => {
+  const rankSchema = new mongoose.Schema({
+    name: String,
+    xpCost: Number,
+    icon: String,
+  });
+
+  const Rank = mongoose.model("Rank", rankSchema);
+
+  return Rank;
+}
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
 
 app.get("/", (req, res) => {
+    console.log(mongo);    
   res.json("Hello World!");
 });
 
 app.listen(PORT, () => {
+  initSchema();
   console.log(`Server is running at http://${HOST}:${PORT}`);
 });
+
+export default mongo;
