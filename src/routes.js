7b45b4bc3e379b28aad56e4de45687eec1aa8691
Juniz <fr.darktrooper08@gmail.com>
2025-04-02 09:52:59 +0200

    feat(posts): implement CRUD operations for posts with authentication middleware

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import securityController from "./controllers/securityController.js";
 import postController from "./controllers/postController.js";
 import fightController from "./controllers/fightController.js";
 import cardController from "./controllers/cardController.js";
+import postController from "./controllers/postController.js";
 
 const router = Router();
 
@@ -31,5 +32,12 @@ router.post("/cards", cardController.addCard);
 router.get("/cards", cardController.getAllCards);
 router.patch("/cards/:id", cardController.updateCard);
 router.delete("/cards/:id", cardController.deleteCard);
+/** Posts */
+router.get('/posts', postController.posts);
+router.post('/posts', authMiddleware, postController.create);
+router.get('/posts/:id', authMiddleware, postController.show);
+router.patch('/posts/:id', authMiddleware, postController.update);
+router.delete('/posts/:id', authMiddleware, postController.delete);
+
 
 export default router;
