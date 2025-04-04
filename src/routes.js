01484741884813e6a0859d1bf31f4ee78021a3a0
Juniz <fr.darktrooper08@gmail.com>
2025-04-04 10:39:59 +0200

    fix(merge): problem fusion routes

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,6 @@ import securityController from "./controllers/securityController.js";
 import postController from "./controllers/postController.js";
 import fightController from "./controllers/fightController.js";
 import cardController from "./controllers/cardController.js";
-import postController from "./controllers/postController.js";
 
 const router = Router();
 
@@ -32,12 +31,5 @@ router.post("/cards", cardController.addCard);
 router.get("/cards", cardController.getAllCards);
 router.patch("/cards/:id", cardController.updateCard);
 router.delete("/cards/:id", cardController.deleteCard);
-/** Posts */
-router.get('/posts', postController.posts);
-router.post('/posts', authMiddleware, postController.create);
-router.get('/posts/:id', authMiddleware, postController.show);
-router.patch('/posts/:id', authMiddleware, postController.update);
-router.delete('/posts/:id', authMiddleware, postController.delete);
-
 
 export default router;
